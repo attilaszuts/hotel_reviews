f4397dc6ae75003d14ad5abcb6cdc34571595084
nobody <no@body.com>
2021-05-21 11:11:51 +0000

    add renderUI elements to server

--- server.R
+++ server.R
@@ -1,5 +1,33 @@
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  params <- reactiveValues()
+  
+  observeEvent(input$tab, {
+    
+    # Welcome page
+    output$welcome <- renderUI({
+      h1('hello')
+    })
+    
+    # Filter page
+    output$hotel_chooser <- renderUI({
+      h1('hello filter')
+      # multiInput(
+      #   inputId = "hotel_chooser",
+      #   label = "hotels", 
+      #   choices = NULL,
+      #   choiceNames = ,
+      #   choiceValues = 
+      # )
+    })
+    
+    # Wordcloud page
+    output$wordcloud <- renderUI({
+      h1('hello cloud')
+      read_data()
+    })
+    
+  }) # close observeEvent 'tab'
   
 })
