d6d2219440a54756bf3a1ad7c9aa6b639950a18e
nobody <no@body.com>
2021-05-21 11:12:06 +0000

    add ui elements to ui

--- ui.R
+++ ui.R
@@ -1,20 +1,32 @@
 library(shiny)
 library(shinydashboard)
+library(shinycssloaders)
 
 shinyUI(dashboardPage(
   dashboardHeader(title = "Hotel reviews"
   ), # close Header
   dashboardSidebar(
     sidebarMenu(
-      menuItem('Positive - Negative', tabName = 'pn', icon = icon('concierge-bell'))
-    )
+      id = 'tab',
+      menuItem('Welcome!', tabName = 'welcome', icon = icon('concierge-bell')),
+      menuItem('Set up filters', tabName = 'filter', icon = icon('filter')),
+      menuItem('Wordcloud', tabName = 'cloud', icon = icon('cloud'))
+    ) # close sidebarMenu
   ), # close Sidebar
   dashboardBody(
     tabItems(
       tabItem(
-        tabName = 'pn',
-        h1('hello ceu')
-        ) # close tabItem 'pn'
+        tabName = 'welcome', 
+        withSpinner(uiOutput('welcome'), type = 5, color = "#222d32")
+      ), # close tabItem 'welcome'
+      tabItem(
+        tabName = 'filter',
+        withSpinner(uiOutput('hotel_chooser'), type = 5, color = "#222d32")
+        ), # close tabItem 'filter'
+      tabItem(
+        tabName = 'cloud',
+        withSpinner(uiOutput('wordcloud'), type = 5, color = "#222d32")
+      ) # close tabItem 'cloud'
     ) # close tabItems
   ) # close Body
 ))
